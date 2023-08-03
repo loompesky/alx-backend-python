53604be66769125e3529888728b211c7ab649a97
loompesky <loompesky@yahoo.com>
2023-08-03 23:17:26 +0000

    VA

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python3
-"""
-    Function: to_kv()
-
-"""
 from typing import Union, Tuple
 
 
-
-def to_kv(k: str, v: union[int, float]) -> Tuple[str, float]:
-    """
-    Function: to_kv()
-
-    Description:
-    Converts a key-value pair into a tuple.
-
-    Parameters:
-    - k (str): The key.
-    - v (int or float): The value, which can be either an integer or a float.
-
-    Return Type:
-    tuple: A tuple containing the key-value pair.
-    """
-
-    return (k, v * v)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    return (k, float(v**2))
