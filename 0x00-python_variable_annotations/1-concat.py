542875524e0812e7ad100dbbb2d66cdd90639cbe
loompesky <loompesky@yahoo.com>
2023-08-03 22:49:47 +0000

    Python - Variable Annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Function: concat()
+
+"""
+
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Function: concat()
+
+    Description:
+    The concat() function is a Python function that
+    takes two integer arguments str1 and str2.
+    Parameters:
+    - str1 (str): The first string argument to be concated.
+    - str2 (str): The second string argument to be concated.
+
+    Return Type:
+    float: The sum of a and b as a floating-point number.
+    """
     return str1 + str2
