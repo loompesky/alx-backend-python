32edbfd47af242d35c9f2b34c016f085fc6bfc5a
loompesky <loompesky@yahoo.com>
2023-08-07 19:02:37 +0000

    AP

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+"""
+Function returns an asyncio.Task
+"""
+
 import asyncio
-from typing import List, Tuple
-from random import uniform
+import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """
+    Calls a randomly generated wait_time function and returns a task
+
+    Args:
+        max_delay: Maximum number of seconds function delays
+
+    Return: An asyncio.Task object
+    """
     return asyncio.create_task(wait_random(max_delay))
