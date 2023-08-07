ddf25b456f9898ead61905bf16bdf0656fcff129
loompesky <loompesky@yahoo.com>
2023-08-07 19:00:50 +0000

    AP

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
-import time
+"""
+Calculate the execution time of 1 co-routine function
+"""
+
 import asyncio
+import random
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    start_time = time.time()
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """
+    Calculates total_time / n for wait_n() execution
+    Which is the average (1 co-routine)
+
+    Args:
+        n: Number of items
+        max_delay: Maximun delay in seconds
+
+    Return: Total time for function to execute
+    """
+
+    elapsed_time: float
+
+    start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    elapsed_time = time.perf_counter() - start_time
+    return elapsed_time / n
